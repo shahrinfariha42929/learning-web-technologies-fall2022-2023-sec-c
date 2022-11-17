ae301839281a4a4ee248f245f6885d728ffe135a
shahrinfariha42929 <shahrinfariha429292gmail.com>
2022-11-18 01:44:54 +0600

    update

---
+++ LAB 4/Tassk4(reg).php
@@ -0,0 +1,26 @@
+<html>
+<head>
+     <title>REGISTRATION</title>
+</head>
+<body>
+<legend><b>Registration</b></legend> </br>
+
+<form action="regcheck.php" method="post">
+Name: <input type="text" name="name"><br>
+Email:  <input type="email" name="email" ><br>
+User Name: <input type="text" name="uname"><br>
+Password: <input type="password" name="pass"><br>
+Confirm Password:<input type="password" name="cpass" ><br>
+Gender:
+<input type="radio" name="gender" value="female">Female
+<input type="radio" name="gender" value="male">Male
+<input type="radio" name="gender" value="other">Other </br>
+Date of Birth :
+<input type="date" name="dob" >
+</br>
+<input type="submit"> 
+<input type="reset"> 
+</form>
+
+</body>
+</html>
---
+++ LAB 4/task3(profile pic).php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Change Password</title>
+</head>
+<body>
+    <form action="checkpic.php" method="POST" >
+		
+         <legend><b>PROFILE PICTURE</b></legend>    
+                    <img src="pp.png" height="80" width="80"></br>
+                    <input type="file" name="pp"> 
+                </br>
+            <input type="submit" name="dpsubmit" value="Submit">
+        
+    </form>
+</body>
+</html>
